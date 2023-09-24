c3493c023e4abf1338606798b953eb5339d886cf
John Sharifi <pooyasharifi.sp2@gmail.com>
2023-09-24 09:56:32 +0330

    feat: add `userLogin` to LoginPage

--- src/pages/login-page/index.tsx
+++ src/pages/login-page/index.tsx
@@ -1,22 +1,54 @@
 import './styles.css'
-export const LoginPage = () => (
-  <main className='login-page'>
-    <section>
-      <form className='login-form'>
-        <h1>ورود</h1>
+import { useState } from 'react'
+import { loginUser } from '../../api'
+export const LoginPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-        <div className='form-group'>
-          <label htmlFor='email'>ایمیل:</label>
-          <input type='email' id='email' />
-        </div>
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const user = {
+      email,
+      password,
+    }
+    loginUser(user)
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 
-        <div className='form-group'>
-          <label htmlFor='password'>رمزعبور:</label>
-          <input type='password' id='password' />
-        </div>
+  return (
+    <main className='login-page'>
+      <section>
+        <form className='login-form' onSubmit={handleSubmit}>
+          <h1>ورود</h1>
 
-        <button type='submit'>ورود</button>
-      </form>
-    </section>
-  </main>
-)
+          <div className='form-group'>
+            <label htmlFor='email'>ایمیل:</label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type='email'
+              id='email'
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='password'>رمزعبور:</label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type='password'
+              id='password'
+            />
+          </div>
+
+          <button type='submit'>ورود</button>
+        </form>
+      </section>
+    </main>
+  )
+}
