2bcd706eeda2054555d3d512999bb31c742d1b15
John Sharifi <pooyasharifi.sp2@gmail.com>
2023-09-24 10:59:02 +0330

    feat: add hint and logic login

--- src/pages/login-page/index.tsx
+++ src/pages/login-page/index.tsx
@@ -1,19 +1,25 @@
 import './styles.css'
 import { useState } from 'react'
 import { loginUser } from '../../api'
+import { LoadingAnimation } from '../../components'
+import { useNavigate } from 'react-router-dom'
+
 export const LoginPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const navigate = useNavigate()
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
     const user = {
       email,
       password,
     }
     loginUser(user)
       .then((res) => {
-        console.log(res)
+        localStorage.setItem('token', JSON.stringify(res.token))
+        navigate('/')
       })
       .catch((error) => {
         console.log(error)
@@ -23,31 +29,40 @@ export const LoginPage = () => {
   return (
     <main className='login-page'>
       <section>
-        <form className='login-form' onSubmit={handleSubmit}>
-          <h1>ورود</h1>
+        <div>
+          <form className='login-form' onSubmit={handleSubmit}>
+            <h1>ورود</h1>
 
-          <div className='form-group'>
-            <label htmlFor='email'>ایمیل:</label>
-            <input
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              type='email'
-              id='email'
-            />
+            <div className='form-group'>
+              <label htmlFor='email'>ایمیل:</label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type='email'
+                id='email'
+              />
+            </div>
+
+            <div className='form-group'>
+              <label htmlFor='password'>رمزعبور:</label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type='password'
+                id='password'
+              />
+            </div>
+
+            <button type='submit'>ورود</button>
+          </form>
+          <div className='hint-wrapper'>
+            <small className='hint'>
+              برای ورود از ایمیل: eve.holt@reqres.in استفاده بکنید.
+            </small>
+            <small>برای رمز عبور از : cityslicka استفاده بکنید.</small>
           </div>
-
-          <div className='form-group'>
-            <label htmlFor='password'>رمزعبور:</label>
-            <input
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              type='password'
-              id='password'
-            />
-          </div>
-
-          <button type='submit'>ورود</button>
-        </form>
+        </div>
+        <LoadingAnimation />
       </section>
     </main>
   )
